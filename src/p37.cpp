225b50dccaa594cd091f70eb9511dff21d325e08
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-04-16 21:15:10 -0500

    Templating and 37

---
+++ src/p37.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#define MAX_BUF 512
+#define println(X) std::cout << X << std::endl
+
+class stopwatch {
+    private:
+        time_t startT;
+        time_t finishT;
+
+    public:
+        double getDuration() { return (finishT - startT); }
+        void start() { 
+            startT  = time(NULL);   
+        }
+        void stop()  { 
+            finishT = time(NULL); 
+        }
+        void show() {
+            println("It took you " << getDuration() << " seconds to hit enter.");
+        }
+        ~stopwatch() {
+            show();
+        }
+};
+void getInput() {
+    println("Please press enter.");
+    scanf("%*c");
+}
+
+int main(int argc, char *argv[]) {
+    stopwatch swatch;
+    swatch.start();
+
+    getInput();
+
+    swatch.stop();
+
+}
--- src/tempcpp.cpp
+++ src/tempcpp.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 #define MAX_BUF 512
+#define println(X) cout << X << "\n"
 
 int main(int argc, char *argv[]) {
     if(argc != 2) {
