4b3f90e22e784df7ba1a13464688dc7acfee5fe7
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-04-30 12:28:25 -0500

    169 done

--- src/p169.cpp
+++ src/p169.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <iostream>
 #include <stdlib.h>
 #include "common.h"
@@ -27,6 +28,12 @@ class coord {
             return y;
         }
 
+        char* getFormatted() {
+            char *ret = new char[MAX_BUF];
+            sprintf(ret, "(%d, %d)", getX(), getY());
+            return ret;
+        }
+
         coord& operator++() {
             ++x;
             ++y;
@@ -38,16 +45,80 @@ class coord {
             ++(*this);
             return *old;
         }
+
+        coord& operator--() {
+            --x;
+            --y;
+            return *this;
+        }
+
+        coord operator--(int) {
+            coord *old = new coord(x, y);
+            --(*this);
+            return *old;
+        }
+
+        bool operator<(coord& b) {
+            return ((this->getX() < b.getX()) && (this->getY() < b.getY()));
+        }
+
+        bool operator>(coord& b) {
+            return ((this->getX() > b.getX()) && (this->getY() > b.getY()));
+        }
+
+        coord operator*(coord& b) {
+            return *(new coord(this->getX() * b.getX(), this->getY() * b.getY()));
+        }
+        
+        coord operator+() {
+            return *(new coord(abs(this->getX()), abs(this->getY())));
+        }
+
+        coord operator+(coord b) {
+            return *(new coord(this->getX() + b.getX(), this->getY() + b.getY()));
+        }
 };
 
 int main(int argc, char *argv[]) {
-    coord *iCoord;
+    coord iCoord, iCoord2;
     
-    demo_void(iCoord = new coord(1, 4));
-    demo_int(iCoord->getX());
-    demo_int(iCoord->getY());
+    demo_void(iCoord = *(new coord(1, -4)));
+    demo_string(iCoord.getFormatted());
     
-    demo_void( iCoord->operator++() );
-    demo_int(iCoord->getX());
-    demo_int(iCoord->getY());
+    println("");
+    demo_void(iCoord2 = iCoord++);
+    demo_string(iCoord2.getFormatted());
+    demo_string(iCoord.getFormatted());
+    
+    println("");
+    demo_void(iCoord = iCoord2--);
+    demo_string(iCoord.getFormatted());
+    demo_string(iCoord2.getFormatted());
+
+    println("");
+    demo_void(iCoord = *(new coord(-573, 7)));
+    demo_string(iCoord.getFormatted());
+
+    println("");
+    demo_string((+iCoord).getFormatted());
+
+    println("");
+    demo_string(iCoord.getFormatted());
+    demo_string(iCoord2.getFormatted());
+    demo_string((iCoord * iCoord2).getFormatted());
+
+    println("");
+    demo_string(iCoord.getFormatted());
+    demo_string(iCoord2.getFormatted());
+    println("");
+    demo_bool(iCoord > iCoord2);
+
+    println("");
+    demo_bool(iCoord < iCoord2);
+
+    println("");
+    demo_bool(+iCoord < iCoord2);
+
+    println("");
+    demo_bool(+iCoord > iCoord2);
 }
