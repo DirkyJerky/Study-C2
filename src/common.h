086ad0b10972ae1376c02bcff2b31e6785389439
Geoff Yoerger <geoffreyiy1@gmail.com>
2015-04-30 12:28:12 -0500

    demo_bool()

--- src/common.h
+++ src/common.h
@@ -2,10 +2,12 @@
 
 #define demo_int(X) printf( #X " -> %d\n", (X) )
 char *___ret;
+bool ___retb;
 #define demo_string(X) ___ret = (X); printf( #X " -> \"%s\"\n", ((___ret == NULL) ? "NULL" : ___ret) )
 #define demo_float(X) printf( #X " -> %f\n", (X) )
 #define demo_void(X) printf( #X "\n" ); X
 #define demo_char(X) printf( #X " -> \"%c\"\n", (X) )
 #define demo_pointer(X) printf( #X " -> \"%p\"\n", (X) )
+#define demo_bool(X) ___retb = (X); printf( #X " -> %s\n", ((___retb) ? "true" : "false" ))
 
 #define demo(X) demo_int(X)
